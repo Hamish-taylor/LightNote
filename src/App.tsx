074a13abaf73235e429f008abaf9e719e86d35e4
hamish <hamisht11@gmail.com>
2022-07-18 23:29:52 +1200

    Added file browser with resize bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
@@ -6,12 +6,14 @@ import { appWindow } from '@tauri-apps/api/window'
 import { MdClear, MdMinimize, MdReadMore, MdKeyboardArrowLeft, MdMenuOpen, MdMenu, MdFolderOpen, MdOutlineInsertDriveFile, MdOutlineCreateNewFolder } from 'react-icons/md'
 import { VscChromeMaximize } from 'react-icons/vsc'
 import { readDir, BaseDirectory, FileEntry } from '@tauri-apps/api/fs';
-
+import { emit, listen } from '@tauri-apps/api/event'
 
 function App() {
   const [count, setCount] = useState(0)
   //const [mainFolder, setMainFolder] = useState<folder>(new folder({ name: "Main", path: "C:/Users/Hamis/Documents/NotesCopy" }))
   const [allPaths, setAllPaths] = useState<FileEntry[]>([])
+  const [showFileLeaf, setShowFileLeaf] = useState(false)
+  const [folderLeafWidth, setFolderLeafWidth] = useState(200)
   //Setting the window to frameless
   appWindow.setDecorations(false);
 
@@ -19,27 +21,13 @@ function App() {
   const maximize = () => appWindow.toggleMaximize();
   const close = () => appWindow.close();
 
-
-
   const showFileBrowserLeaf = () => {
-    let value = document.getElementById('FileBrowserLeaf')!.style.left === '0px';
-    console.log(value);
-
-
-    if (value) {
-      document.getElementById('FileBrowserLeaf')!.style.maxWidth = '0px'
-      let dx = document.getElementById('FileBrowserLeaf')!.offsetWidth;
-      document.getElementById('FileBrowserLeaf')!.style.left = '-' + dx + 'px'
-    }
-    else {
-      document.getElementById('FileBrowserLeaf')!.style.maxWidth = '100%'
-      document.getElementById('FileBrowserLeaf')!.style.left = '0px'
-    }
+    setShowFileLeaf(!showFileLeaf);
   };
 
   const readFiles = async () => {
-    const entries = await readDir('C:/Users/Hamis/Documents/NotesCopy', {recursive: true });
-    setAllPaths( allPaths.concat(entries));
+    const entries = await readDir('C:/Users/Hamis/Documents/NotesCopy', { recursive: true });
+    setAllPaths(allPaths.concat(entries));
     function processEntries(entries: any) {
       for (const entry of entries) {
         console.log(`Entry: ${entry.path}`);
@@ -48,125 +36,148 @@ function App() {
         }
       }
     }
-     processEntries(entries);
+    processEntries(entries);
   }
-  //readFiles();
 
-  // const  renderFolders = async (dir : string) => {
-  //     const entries = await readDir(dir, {recursive: true });
 
-  //   return (
-  //     <>
-  //     <div> dir</div>
-  //     {entries.map(async (entry: any) => {
-  //       if (entry.type === 'file') {
-  //         return (
-  //           <div>
-  //             <div>{entry.name}</div>
-  //           </div>
-  //         )
-  //       }
-  //       else {
-  //         return (
-  //           <div>
-  //             <div>{entry.name}</div>
-  //             <div>{await renderFolders(entry.path)}</div>
-  //           </div>
-  //         )
-  //       }
-  //     }
-  //     )}    
-  //     </>
-  //     )
-  // }
-
-  const renderFolders =  () => {
+  const renderFolders = () => {
     return (
-      <>
-      <div> dir</div>
-      {allPaths.map(async (entry: any) => {
-        if (entry.type === 'file') {
-          return (
-            <div>
-              <div>{entry.name}</div>
-            </div>
-          )
+      <div>
+        <div> dir</div>
+        {allPaths.map((entry: any) => {
+          if (entry.type === 'file') {
+            return (
+              <div>
+                <div>{entry.name}</div>
+              </div>
+            )
+          }
+          else {
+            return (
+              <div>
+                <div>{entry.name}</div>
+                <div>{entry.path}</div>
+              </div>
+            )
+          }
         }
-        else {
-          return (
-            <div>
-              <div>{entry.name}</div>
-              <div>{entry.path}</div>
-            </div>
-          )
-        }
-      }
-      )}
-      </>
-      )
+        )}
+      </div>
+    )
   }
 
 
+  useEffect(() => {
+    readFiles();
 
+    // Query the element
+    const resizer = document.getElementById('resizeBar')!;
+    const leftSide = document.getElementById('FileBrowserLeaf')!;
+    const rightSide = document.getElementById('contentPane')!;
 
-  // const renderFolder = (folder : folder, indent : number) => {
-  //   return (    
-  //   <>
-  //        {/* {indent != 0 ?  <div className='indent'></div>  : null}  */}
+    // The current position of mouse
+    let x = 0;
+    let y = 0;
 
-  //     <div id={String(folder.name)} style={{width: "100%", display: 'none'}}  className="folder">
-  //       {folder.folders.map((fol) => {
-  //           return (
-  //             <>
-  //           <button className='file-button' onClick={() => {showFolder(fol)}}> {openedFolders.includes(fol.path) ? < IoMdArrowDropdown/>  :   <IoMdArrowDropright/>}{ " " + fol.name}</Button >   
+    // Width of left side
+    let leftWidth = 0;
 
-  //           { renderFolder(fol, indent + 1)}
-  //           </>
-  //            )
-  //       })}
-  //       {folder.files.map(file => {
+    // Handle the mousedown event
+    // that's triggered when user drags the resizer
+    const mouseDownHandler = function (e: { clientX: number; clientY: number }) {
+      // Get the current mouse position
+      x = e.clientX;
+      y = e.clientY;
+      leftWidth = parseInt(getComputedStyle(leftSide, '').width); //leftSide.getBoundingClientRect().width;
 
-  //           return openedFile === String(folder.path) + '/' +  String(file) ?
-  //           <button className='file-button-toggle' onClick={() => {
+      // Attach the listeners to `document`
+      document.addEventListener('mousemove', mouseMoveHandler);
+      document.addEventListener('mouseup', mouseUpHandler);
+    };
 
-  //           }}>{file}</button>
-  //           :
-  //           <button className='file-button'  onClick={() => {
-  //             _setOpenedFile(folder,file),
+    // Attach the handler
+    resizer.addEventListener('mousedown', mouseDownHandler);
 
-  //             window.electron.ipcRenderer.getFile(folder.path + '/'+file)   
-  //           }}>{file}</button>
-  //       })}
-  //   </div>
-  //   </>
-  //     )
-  // }
+    const mouseMoveHandler = function (e: { clientX: number; clientY: number }) {
+      // How far the mouse has been moved
+      const dx = e.clientX - x;
 
+      let newLeftWidth = ((leftWidth + dx) * 100) / resizer.parentElement!.getBoundingClientRect().width;
+      if (newLeftWidth < 4) {
+        newLeftWidth = 0;
+      }
 
+      leftSide.style.width = `${newLeftWidth}%`;
 
+      document.body.style.cursor = 'col-resize';
+      document.body.style.cursor = 'col-resize';
+
+      leftSide.style.userSelect = 'none';
+      leftSide.style.pointerEvents = 'none';
+
+      rightSide.style.userSelect = 'none';
+      rightSide.style.pointerEvents = 'none';
+    };
+
+    const mouseUpHandler = function () {
+      resizer.style.removeProperty('cursor');
+      document.body.style.removeProperty('cursor');
+
+      leftSide.style.removeProperty('user-select');
+      leftSide.style.removeProperty('pointer-events');
+
+      rightSide.style.removeProperty('user-select');
+      rightSide.style.removeProperty('pointer-events');
+
+      // Remove the handlers of `mousemove` and `mouseup`
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+
+      leftWidth = parseInt(getComputedStyle(leftSide, '').width);
+      if (leftWidth < 4) {
+        setShowFileLeaf(false);
+        setFolderLeafWidth(200);
+      } else {
+        setFolderLeafWidth(leftWidth);
+        setShowFileLeaf(true);
+      }
+      if (leftWidth > 4 && showFileLeaf == false) {
+        setShowFileLeaf(true);
+      }
+    };
+
+  }, []);
 
   return (
-    <div className="relative flex  w-screen flex-col">
+    <div className="relative flex  w-screen flex-col" >
 
-      <div data-tauri-drag-region className="flex flex-1 w-screen right-0 left-0 justify-end bg-slate-600">
-        <button onClick={minimize} className="bg-sky-600 hover:bg-sky-700 rounded-none border-none focus:outline-none bg-center" > <MdMinimize /> </button>
-        <button onClick={maximize} className="bg-sky-600 hover:bg-sky-700 rounded-none border-none focus:outline-none"> <VscChromeMaximize /> </button>
-        <button onClick={close} className="bg-sky-600 hover:bg-sky-700 rounded-none border-none focus:outline-none"> <MdClear /> </button >
+      <div data-tauri-drag-region className="flex  w-screen right-0 left-0 justify-end bg-zinc-900">
+        <button onClick={minimize} className="bg-zinc-900 hover:bg-zinc-600 rounded-none border-none focus:outline-none bg-center" > <MdMinimize className='flex-1'/> </button>
+        <button onClick={maximize} className="bg-zinc-900 hover:bg-zinc-600 rounded-none border-none focus:outline-none"> <VscChromeMaximize /> </button>
+        <button onClick={close} className="bg-zinc-900 hover:bg-red-600 rounded-none border-none focus:outline-none"> <MdClear /> </button >
       </div>
 
       <div className="relative flex-1 flex flex-row">
-        <div className="z-10 h-screen bg-sky-600">
-          <button onClick={showFileBrowserLeaf} className="bg-sky-600 hover:bg-sky-700 rounded-none border-none focus:outline-none flex-1"> <MdMenuOpen /> </button>
+        <div className="z-10 h-screen bg-zinc-900">
+          <button id="fileBrowser" onClick={showFileBrowserLeaf} className="text-lg text-zinc-500 hover:text-white bg-zinc-900 hover:bg-zinc-900 rounded-none border-none focus:outline-none flex-1 "> {showFileLeaf ? <MdMenuOpen /> : <MdMenu />} </button>
         </div>
-        <div className=" z-0 relative duration-150 ease-in-outh-screen " id='FileBrowserLeaf'>
-          <div className=" bg-white w-full h-1/3"></div>
-          <div className='bg-black w-full h-2/3  id="file-container'>
-            {//renderFolders()
+        <div style={showFileLeaf ? { width: folderLeafWidth + 'px' } : { width: '0px' }} className=" z-0 relative h-screen flex flex-col overflow-hidden resize-x" id='FileBrowserLeaf'>
+          <div className="  w-full justify-center flex bg-zinc-800 p-3">
+            <button className="px-4 py-1 text-2xl  bg-zinc-800 text-zinc-500 font-semibold rounded-none border-none hover:text-white   focus:outline-none "><MdOutlineInsertDriveFile/></button>
+            <button className="px-4 py-1 text-2xl bg-zinc-800 text-zinc-500 font-semibold rounded-none border-none hover:text-white   focus:outline-none "><MdOutlineCreateNewFolder/></button>
+          </div>
+          <div className='bg-zinc-800 w-full   id="file-container  flex-1 overflow-auto'>
+            {renderFolders()
             }
           </div>
 
         </div>
-        <div className='flex-1'>
+        <div id="resizeBar" className="w-1 hover:bg-sky-400 cursor-col-resize ease-in-out duration-300 delay-50">
+
+        </div>
+        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        </div>
+        <div id="contentPane" className='flex flex-1'>
           <div className='flex-1'>
             <a target="_blank">
               <img src="/vite.svg" className="logo" alt="Vite logo" />
